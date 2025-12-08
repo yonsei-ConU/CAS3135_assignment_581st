65c07fd4ebca2593c70afbca35bbc3d3d3a25f1b
yonsei-conu <jaeyeol.lee@gmail.com>
2025-12-08 19:49:53 +0900

    12-01 until page8

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,24 @@
+terraform {
+    backend "s3" {
+        bucket         = "cas3135-2025-tfstates"
+        key            = "cas3135-2024148081/calc-k8s"
+        dynamodb_table = "cas3135-terraform-locks"
+        region         = "ap-northeast-2"
+        encrypt        = true
+    }
+    required_providers {
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = "~> 2.29"
+        }
+    }
+    required_version = ">= 1.5.0"
+}
+
+provider "kubernetes" {
+    config_path = var.kubernetes_config_path
+}
+
 resource "kubernetes_config_map" "backend-config" {
   metadata {
 		name = "backend-config"
